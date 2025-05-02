61a17930c24d8f6cd2ec519b714a17f3dc73fbc9
DanishCodes1 <muhammadanish.malikz@gmail.com>
2025-05-02 22:10:18 +0500

    Cores Functionality

--- parallel-sssp-update/core/openmp_utils.h
+++ parallel-sssp-update/core/openmp_utils.h
@@ -1,6 +1,15 @@
 #ifndef OPENMP_UTILS_H
 #define OPENMP_UTILS_H
 
-void set_omp_threads(int num_threads);
-
+#ifdef _OPENMP
+#include <omp.h>
 #endif
+
+// Set OpenMP thread count
+inline void set_omp_threads(int n) {
+#ifdef _OPENMP
+    omp_set_num_threads(n);
+#endif
+}
+
+#endif // OPENMP_UTILS_H
